d016067f0cccad79c93e86b8fd15d292d4753192
mkkim <infinitelove367@gmail.com>
2024-10-15 15:03:01 +0900

    🍎 Chore: 깃랩 자동화 추가
    #
    
    
    Former-commit-id: dbc8ac07bb4a8e6badade47306a78b31823d9a91

--- commitlint.config.ts
+++ commitlint.config.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c36c1f15938aa67b271b50a6c6f6c7ae4aefd822428a44aab0bbe6424ed2e1ed
+oid sha256:382d43e5bab6887d31b4104e59757bb102a86ce2801d0907f0fcbeb6cde17d51
 size 2432
