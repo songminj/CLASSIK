e451c39726517dd59d88b748160101a4d3ff118e
mkkim <infinitelove367@gmail.com>
2024-11-04 12:47:13 +0900

    ✨ Feat: playpage ãUI 틀 구현
    #
    
    
    Former-commit-id: e22a41d42f90f75fb08acb8c5925999e5d36b8fc

--- Classik/app/src/main/java/com/example/classik/PlaylistActivity.kt
+++ Classik/app/src/main/java/com/example/classik/PlaylistActivity.kt
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0a2a8e30bfa8c46cf5fa79d7697afb07c64bdd2bc5373cb7ea8ea94af2a34fa9
-size 8496
+oid sha256:e19e37000c8424d05525b732e3282e8a7cb68508c11edb885e19e64162626a15
+size 8538
--- Classik/app/src/main/java/com/example/classik/ui/theme/Theme.kt
+++ Classik/app/src/main/java/com/example/classik/ui/theme/Theme.kt
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4d6af205fae1b0eacd2d17ee1a7942c473c42530d697167e54d14e51244dcabd
-size 2059
+oid sha256:64b4479c5e2953252bc67f3a1e38138dd7123db360c95e8970d092edbc76d7f1
+size 2117
