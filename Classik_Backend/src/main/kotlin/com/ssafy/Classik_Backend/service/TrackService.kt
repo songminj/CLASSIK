213ebb88d96bb52acaff1b90dcfdb671497230a3
박동환 <znxn123@naver.com>
2024-11-04 11:15:53 +0900

    ✨ Feat: 특정 Track 조회 Service 구현
    #
    
    
    Former-commit-id: 93b3316a92889c073d460bd78eecc1721f85ddc3

--- Classik_Backend/src/main/kotlin/com/ssafy/Classik_Backend/service/TrackService.kt
+++ Classik_Backend/src/main/kotlin/com/ssafy/Classik_Backend/service/TrackService.kt
@@ -1,7 +1,11 @@
 package com.ssafy.Classik_Backend.service
 
+import com.ssafy.Classik_Backend.domain.track.dto.TrackDto
+import com.ssafy.Classik_Backend.domain.track.entity.Track
+
 interface TrackService {
 
-
+    fun getTrack(id: Long): TrackDto
+    fun searchTracks(word: String): List<Track>
 
 }
--- Classik_Backend/src/main/kotlin/com/ssafy/Classik_Backend/service/TrackServiceImpl.kt
+++ Classik_Backend/src/main/kotlin/com/ssafy/Classik_Backend/service/TrackServiceImpl.kt
@@ -1,8 +1,3 @@
-package com.ssafy.Classik_Backend.service
-
-import org.springframework.stereotype.Service
-
-@Service
-class TrackServiceImpl {
-
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:2360bcb272e29a49119cb5baf868ad4b2f6d60dedbb1e4ff4ad4920fd7d697f5
+size 760
