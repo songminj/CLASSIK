004d1fe087e53bd54cbb6e837207f75573fd3ddd
박동환 <znxn123@naver.com>
2024-11-04 16:06:44 +0900

    🧠 Refactor: Class package 구분
    Former-commit-id: 1402d06d92b8d31150694b9b6cfa313dc22eeb7c

--- Classik_Tracks_Crawling/src/main/java/com/ssafy/a604/Main.java
+++ Classik_Tracks_Crawling/src/main/java/com/ssafy/a604/Main.java
@@ -1,9 +1,11 @@
 package com.ssafy.a604;
 
+import com.ssafy.a604.crawling.CrawlingClassic;
+
 public class Main {
 
     public static void main(String[] args) {
-            new Crawling().startCrawling();
+            new CrawlingClassic().startCrawling();
     }
 
 }
