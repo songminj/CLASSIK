dd7070509557f3ddc3fd639e06ca374b86629d82
박동환 <znxn123@naver.com>
2024-11-04 18:57:30 +0900

    🧠 Refactor: User entity 위치 변경
    #
    
    
    Former-commit-id: 7e12f278992f1eea86d89d9803f1829e62cb0e12

--- Classik_Backend/src/main/kotlin/com/ssafy/Classik_Backend/auth/service/CustomUserDetailService.kt
+++ Classik_Backend/src/main/kotlin/com/ssafy/Classik_Backend/auth/service/CustomUserDetailService.kt
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:674e54c95995ca98153923aacf424af46a3f57161b5fb6a2a2bd887ad9fa6c6e
-size 1064
+oid sha256:931a4b6823375cd3d92736e709c9dfcaa93fd6b6f2e24a27e8d5a1c89af76d69
+size 1047
--- Classik_Backend/src/main/kotlin/com/ssafy/Classik_Backend/auth/service/JoinService.kt
+++ Classik_Backend/src/main/kotlin/com/ssafy/Classik_Backend/auth/service/JoinService.kt
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:653f6016c92ae27fb30892e524ea341dd9d157caa13fc62fd404601bbfd8095d
-size 1223
+oid sha256:dc4f4a6c011b7b8f6c3c2cdab39fc52cf21d9471cda1fbdd39deb89809f8b111
+size 1060
