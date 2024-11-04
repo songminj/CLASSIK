be8e9d0889192fcfa49f2cdb973fcf0fdd741290
mkkim <infinitelove367@gmail.com>
2024-11-04 16:51:50 +0900

    ✨ Feat: 재생 페이지 UI 구현
    - 가ì짜 이퀄라이저 구현(배경)
    - 슬라이더 (프로그레스바) 구현
    - 이전체 UI 및 ã페이지 이동 연결
    
    #
    
    
    Former-commit-id: 7ab5f10203d1fd5a5d4bc7635e19b47dc95f1694

--- Classik/app/src/main/java/com/example/classik/PlaylistBar.kt
+++ Classik/app/src/main/java/com/example/classik/PlaylistBar.kt
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e447f89c9f81e51b2cb9b39e16514c1e3ae2d41b4a6f83c650b1be4354ea6030
-size 8467
+oid sha256:c2930bff5a1210f192fac2e5a20dbd98fd6d540e6a017973841f541f5b9eccbc
+size 8587
