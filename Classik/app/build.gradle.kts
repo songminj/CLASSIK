f615bcbf2d86703f95636bbbb09107668c640e81
박준영 <raypark1241@gmail.com>
2024-11-06 14:55:58 +0900

    feat: navigator 패키지 설치
    Former-commit-id: 66f289abb73b1cc72f240b7833f8dc2d8d088eb3

--- Classik/app/build.gradle.kts
+++ Classik/app/build.gradle.kts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0867aeaae7d94f1778af717499c0ed0d96f40f93ab6f7a47aa9f71903d0354df
-size 2517
+oid sha256:74fcac112d970cb573ab4cc711ae79e76621fdfb2e84c118ffbf2c319a3b75b9
+size 2561
