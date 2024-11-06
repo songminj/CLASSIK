3c18b6c4c3bf7d8b0e0c44cdebb709ac9213b465
박동환 <znxn123@naver.com>
2024-11-06 13:05:09 +0900

    ✨ Feat: contextHolder userId 반환 구현
    #
    
    
    Former-commit-id: 107da53caf151994392433ed7fb7767607e9fd7e

--- Classik_Backend/src/main/kotlin/com/ssafy/Classik_Backend/auth/dto/CustomUserDetails.kt
+++ Classik_Backend/src/main/kotlin/com/ssafy/Classik_Backend/auth/dto/CustomUserDetails.kt
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e6975b02d9d48d3ec9752db1e75ffa58e4fc990bc5b49e52453cc5d3883cbe53
-size 1044
+oid sha256:da346b9549275bd5ef3a661321506699c5950595f996199e7d6b2e6ec3556cf5
+size 780
