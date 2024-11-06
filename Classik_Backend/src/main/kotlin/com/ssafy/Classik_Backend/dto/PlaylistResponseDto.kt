44236f44475ad1499192a4a9984897203098ffe5
박동환 <znxn123@naver.com>
2024-11-06 10:59:31 +0900

    ✨ Feat: Track Response Dto 구분
    #
    
    
    Former-commit-id: 969f8467594e0050721b2ad43ffeaf5f840d023d

--- Classik_Backend/src/main/kotlin/com/ssafy/Classik_Backend/dto/PlaylistResponseDto.kt
+++ Classik_Backend/src/main/kotlin/com/ssafy/Classik_Backend/dto/PlaylistResponseDto.kt
@@ -1,6 +1,8 @@
 package com.ssafy.Classik_Backend.dto
 
 data class PlaylistResponseDto(
-    val title: String,
-    val tracks: MutableList<TrackDetailResponseDto>,
+    val playlistTitle: String,
+    val playlistId: Int,
+    val trackCount: Int,
+    val tracks: MutableList<TrackSimpleResponseDto>,
 )
--- Classik_Backend/src/main/kotlin/com/ssafy/Classik_Backend/dto/TrackDetailResponseDto.kt
+++ Classik_Backend/src/main/kotlin/com/ssafy/Classik_Backend/dto/TrackDetailResponseDto.kt
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c072764f3748791a6e0960599e791b06a0fb43feb52a54ae2694dc134980719b
-size 1016
+oid sha256:a62d32ef4eac9204c60ccc37f21db29880e00aba25b5623b55cd47ff4dd9d10e
+size 1022
