9114d1555f2202a8a7d4c7cf8f01972291dc2b21
박동환 <znxn123@naver.com>
2024-11-07 09:43:27 +0900

    🔨 Fix: Playlists 반환 Controller 수정
    #
    
    
    Former-commit-id: 67a40942c5ab96968d9f8ed3f1990e97bb4c3d3b

--- Classik_Backend/src/main/kotlin/com/ssafy/Classik_Backend/controller/PlaylistController.kt
+++ Classik_Backend/src/main/kotlin/com/ssafy/Classik_Backend/controller/PlaylistController.kt
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c75b4774422765c51e079c651be5bc0cf2fceda4f61c4688cb3e785617c89191
-size 1685
+oid sha256:b7e35692f4b93909242c651dfb26c550206435fe5e5bbbfdc4a91d9fb59011b9
+size 1703
