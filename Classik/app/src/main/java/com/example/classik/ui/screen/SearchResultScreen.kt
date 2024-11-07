b84fe1524ac0ef2caff42d4ab816b4d3042edd73
박준영 <raypark1241@gmail.com>
2024-11-07 14:17:51 +0900

    Feat/24 fe ui
    Former-commit-id: 498f92e17cbd5a001f990d7048d0ec220d90b6ef

--- Classik/app/src/main/java/com/example/classik/ui/screen/SearchResultScreen.kt
+++ Classik/app/src/main/java/com/example/classik/ui/screen/SearchResultScreen.kt
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:33d65f211f2a2c5a5c3c2bbf8cd2adb1138168733bbfa802218fdc14a559001b
-size 2746
+oid sha256:1dd9884d325e6110d61d5786d8bbe418e0b8907db3cc37a850918b6624b528b0
+size 2747
