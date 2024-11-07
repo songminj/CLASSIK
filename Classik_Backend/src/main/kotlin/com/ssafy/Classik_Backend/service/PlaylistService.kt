cdf3972fcb6cf48a0cf8af6a24ff3abe25974490
박동환 <znxn123@naver.com>
2024-11-07 09:43:11 +0900

    🔨 Fix: Playlists 반환 데이터 및 로직 수정
    #
    
    
    Former-commit-id: f757be15d8d0bc647a2179c98275d29f37ed4ed1

--- Classik_Backend/src/main/kotlin/com/ssafy/Classik_Backend/service/PlaylistService.kt
+++ Classik_Backend/src/main/kotlin/com/ssafy/Classik_Backend/service/PlaylistService.kt
@@ -5,7 +5,7 @@ import com.ssafy.Classik_Backend.dto.*
 interface PlaylistService {
 
     fun getPlaylist(playlistId: Int): PlaylistResponseDto
-    fun getPlaylists(): List<PlaylistResponseDto>
+    fun getPlaylists(): List<PlaylistThumbnailResponseDto>
     fun createPlaylist(requestDto: PlaylistCreateRequestDto): PlaylistCreateResponseDto
     fun updatePlaylist(playlistId: Int, requestDto: PlaylistUpdateRequestDto): PlaylistResponseDto
     fun deletePlaylist(playlistId: Int)
