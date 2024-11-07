effa4e77e7f45f109b7de40a6f7fbfe61504ff84
박동환 <znxn123@naver.com>
2024-11-07 16:31:25 +0900

    ⭐️ Style: 코드 개선
    #
    
    
    Former-commit-id: 1aeb450ba647d607307816162e8958a15cc35063

--- Classik_Backend/src/main/kotlin/com/ssafy/Classik_Backend/dto/PlaylistThumbnailResponseDto.kt
+++ Classik_Backend/src/main/kotlin/com/ssafy/Classik_Backend/dto/PlaylistThumbnailResponseDto.kt
@@ -1,10 +1,8 @@
 package com.ssafy.Classik_Backend.dto
 
 data class PlaylistThumbnailResponseDto(
-
     val playlistId: Int,
     val playlistTitle: String,
     val trackCount: Int,
     val thumbnailUrls: List<String?>,
-
 )
--- Classik_Backend/src/main/kotlin/com/ssafy/Classik_Backend/service/PlaybackHistoryServiceImpl.kt
+++ Classik_Backend/src/main/kotlin/com/ssafy/Classik_Backend/service/PlaybackHistoryServiceImpl.kt
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7e999d6a2ca75ccd6f8b64c599387103f0d62353799581fbf4b603425fa158ef
-size 1748
+oid sha256:6b8425586255e150eefaa8a6668ce7c92089d42ce380bea70078d6066ef8e33c
+size 1734
