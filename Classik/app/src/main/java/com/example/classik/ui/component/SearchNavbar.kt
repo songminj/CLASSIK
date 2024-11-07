f14e8343d542113d4418322ea1f70ed8c42ec161
박준영 <raypark1241@gmail.com>
2024-11-08 08:49:25 +0900

    fix: 여러가지
    Former-commit-id: 97943f97753ce2b6d9d1dbadbab1da1647d0daee

--- Classik/app/src/main/java/com/example/classik/ui/component/SearchNavbar.kt
+++ Classik/app/src/main/java/com/example/classik/ui/component/SearchNavbar.kt
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b8d3fcc4e105b66ba3f70060796c941c31669990bb7f3db3ffde672c711ee4b3
-size 3006
+oid sha256:0db6eb51b02b90a04f8c051f68358207af021971ca28dc4cc9be372d243c3007
+size 3194
