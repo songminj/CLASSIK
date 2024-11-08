6ddebcf5c365de230a2c917b43ea5957b222c831
박동환 <znxn123@naver.com>
2024-11-08 13:29:08 +0900

    ✨ Feat: REDIS_KEY_PREFIX 변수 접근 변경
    #
    
    
    Former-commit-id: 0c118140e2ce7b3587fd7d487d492c11c2a157bc

--- Classik_Backend/src/main/kotlin/com/ssafy/Classik_Backend/service/PlaybackHistoryServiceImpl.kt
+++ Classik_Backend/src/main/kotlin/com/ssafy/Classik_Backend/service/PlaybackHistoryServiceImpl.kt
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6b8425586255e150eefaa8a6668ce7c92089d42ce380bea70078d6066ef8e33c
-size 1734
+oid sha256:eeb240876e1e51573e71d11536f879c0723a7d952c5f9bb9fddf7a53fc129b89
+size 1644
