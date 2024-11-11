141675bb9337c207231d37777c1b2a1035f36136
송민정 <mjm6k.naver.com>
2024-11-11 16:23:43 +0900

    🧠 : 자동화 코드 refactor
    Former-commit-id: 695a23a0f2ab4492c5a61e111c752750a54e9cac

--- Classik_AI/searchopenai/skybox.py
+++ Classik_AI/searchopenai/skybox.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0d7db5881068781f3c4c9bb30127cbefed73576d9760840f85a2a22d674a4cfb
-size 5447
+oid sha256:d0bac0f0078bbff2d6dbbbe8728550f9c10b1e880d559b3271557df05294f2b5
+size 7991
