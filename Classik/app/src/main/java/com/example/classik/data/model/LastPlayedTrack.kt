197d7df23d21cbc2e095fa0423b4247e8de33126
mkkim <infinitelove367@gmail.com>
2024-11-11 13:47:03 +0900

    ✨ Feat: í플레이리스트 컴포넌트 api 연결 및 관련 기능 구현
    #
    
    
    Former-commit-id: cdd9d67611ad6e44552845ccdb9517232919fad9

--- Classik/app/src/main/java/com/example/classik/data/model/LastPlayedTrack.kt
+++ Classik/app/src/main/java/com/example/classik/data/model/LastPlayedTrack.kt
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:48a50e2d188ba29544e4fbdcf3a864c6c8f8e8de5bc19c610621d5cd050a22e8
-size 742
+oid sha256:9be00f68c0022edca52774d417c4643997a821bfb25054ee8f32e8edd5e3ab98
+size 744
--- Classik/app/src/main/java/com/example/classik/data/model/Tags.kt
+++
@@ -1,6 +0,0 @@
-package com.example.classik.data.model
-
-data class Tags(
-    val type: String,
-    val content: String
-)
