3b9d504ac8e63dbd957c586e397bfc1c6671a44f
박준영 <raypark1241@gmail.com>
2024-11-11 16:25:32 +0900

    feat: user관련 api호출, 데이터 모델 추가
    Former-commit-id: 1ea752126043490f1b606c40ea2fcc07bf2a8617

--- Classik/app/src/main/java/com/example/classik/network/UserApiService.kt
+++ Classik/app/src/main/java/com/example/classik/network/UserApiService.kt
@@ -1,5 +1,3 @@
-package com.example.classik.network
-
-interface UserApiService {
-
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:905199ed42b0e3f6ff9362f5a8a00f26a2f3a1d92f87d0ec6036f3ee69eecc60
+size 1155
---
+++ Classik/app/src/main/java/com/example/classik/viewmodel/UserViewModel.kt
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:b837b97e208ccc68a80702524deebce80769664c4df72c8c28eca5a3edf69922
+size 959
