8592d63f5bad473f4f58773e46cc5da10a2a43a7
박준영 <raypark1241@gmail.com>
2024-11-12 15:01:00 +0900

    feat: login api 연결
    Former-commit-id: 6ff7b7b755dd54ed444979634cbeca1a39324170

--- Classik/app/src/main/java/com/example/classik/viewmodel/UserViewModel.kt
+++ Classik/app/src/main/java/com/example/classik/viewmodel/UserViewModel.kt
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:96163f821062f3c48048137f4e70017c9e22460c8d188f295cd381d80191fcea
-size 927
+oid sha256:db91688a7df980df99ae1db4750c850553edf6fc2c5153bbe6d387699d5c2151
+size 1801
