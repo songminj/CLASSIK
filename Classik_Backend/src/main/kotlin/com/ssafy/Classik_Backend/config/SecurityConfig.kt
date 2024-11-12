64516e445d56ac1e991ffb71b2116d6e86916270
ttaeram <ryu.taeram@gmail.com>
2024-11-12 13:47:06 +0900

    ⭐️ Style: Security Config route added
    Former-commit-id: 9834173d2a9839921b7fe99552d87e15256a9606

--- Classik_Backend/src/main/kotlin/com/ssafy/Classik_Backend/config/SecurityConfig.kt
+++ Classik_Backend/src/main/kotlin/com/ssafy/Classik_Backend/config/SecurityConfig.kt
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c7faec9bff898c5e809f0765e564f814b907f6b0803b16672ce08b0234ce0dbd
-size 3915
+oid sha256:772d52659dec34a149a75658bff4a1bfcd9069e723c12ed3a736b0e67d67cb56
+size 4003
