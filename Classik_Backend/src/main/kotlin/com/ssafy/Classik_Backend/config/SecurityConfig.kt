1779cbd0c0426f7a3b3d886128752ff8beef04b8
유성주 <tjdwn6753@naver.com>
2024-11-12 16:17:43 +0900

    securityconfig수정
    Former-commit-id: 6989399675b55dfc6337b45b73b261a86ed3de35

--- Classik_Backend/src/main/kotlin/com/ssafy/Classik_Backend/config/SecurityConfig.kt
+++ Classik_Backend/src/main/kotlin/com/ssafy/Classik_Backend/config/SecurityConfig.kt
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c715819d831c682de54ffe4d3dab5d2102e826376522d1461b59b2030cb9e752
-size 4002
+oid sha256:be0186cd5475bd93759e9c5595dae94c60016c36fbaec52c220eba0fe3587303
+size 4022
