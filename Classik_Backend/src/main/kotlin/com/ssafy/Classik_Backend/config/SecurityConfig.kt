569c9e9cc3d30f5a21fd498bf7e219a5e2d8d89b
ttaeram <ryu.taeram@gmail.com>
2024-11-13 09:23:41 +0900

    ⭐️ Style: api route deleted
    Former-commit-id: a817fa7c81d2488cd474f198a6ea73c73f0cc904

--- Classik_Backend/src/main/kotlin/com/ssafy/Classik_Backend/config/SecurityConfig.kt
+++ Classik_Backend/src/main/kotlin/com/ssafy/Classik_Backend/config/SecurityConfig.kt
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4559952cfbcd848491336864ac0334aae8006ff92a7071b4965041d6cd750cbf
-size 3879
+oid sha256:22fa5036e2b0fc61d6a2ec96a4355e942884d24e1418d43f6b7c8b1cb1011fd3
+size 3835
