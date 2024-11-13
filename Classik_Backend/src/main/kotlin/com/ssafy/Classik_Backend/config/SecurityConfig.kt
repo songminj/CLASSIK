e68c6693cb4ec349fc9a6ac9cf57f3fb6b4c6837
ttaeram <ryu.taeram@gmail.com>
2024-11-13 18:03:37 +0900

    Refactor: Login
    Former-commit-id: 6bfbcfa422ae2282724a492a87375f785fbe4942

--- Classik_Backend/src/main/kotlin/com/ssafy/Classik_Backend/config/SecurityConfig.kt
+++ Classik_Backend/src/main/kotlin/com/ssafy/Classik_Backend/config/SecurityConfig.kt
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:97c0e8c8b1fe42d3a23bd10d18ac0e4c8fbb0f11a519283a8b90cc1154282745
-size 4874
+oid sha256:cd2e618581ab6b8928293ed7be80f553c5996895e434ef8e3c8f134104866cff
+size 4884
